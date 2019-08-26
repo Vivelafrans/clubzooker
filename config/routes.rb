b13f7fc297bc8f3a441ac22d8daf10938748a578
Frans Knaapen <fransknaapen@gmail.com>
2019-08-26 16:41:17 +0200

    Added the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  resources :clubs do
+    resources :offers, except: [:show]
+    resources :memberships, only: [:new, :create, :destroy, :update, :edit, :index]
+  end
+
+  resources :interests, except: [:show, :index]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
