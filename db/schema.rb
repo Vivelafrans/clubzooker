82be5da872568aea20aa6936e05264d696a6032c
Leo Polwein <leo.polwein@gmx.de>
2019-08-27 12:20:25 +0200

    Implemented Cloudinary and Carrierwave

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_26_130036) do
+ActiveRecord::Schema.define(version: 2019_08_27_101759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_130036) do
     t.string "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["user_id"], name: "index_clubs_on_user_id"
   end
 
@@ -79,6 +80,7 @@ ActiveRecord::Schema.define(version: 2019_08_26_130036) do
     t.string "longitude"
     t.string "latitude"
     t.bigint "club_id"
+    t.string "photo"
     t.index ["club_id"], name: "index_users_on_club_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
