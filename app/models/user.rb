6aac6fc50eb77a7fc67c3b146e18263f077c33bc
Leo Polwein <leo.polwein@gmx.de>
2019-08-27 11:50:42 +0200

    added validations and class relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  belongs_to :club, class_name: "administered_club"
+  has_many :memberships
+  has_many :interests
+
+  validates :name, presence: true
+  validates :age, presence: true, inclusion: { in: 14..120, message: "%{value} is not a valid age" }
+  validates :address, presence: true
+  validates :description, length: { maximum: 500 }
+  validates :club_id, uniqueness: true
 end
