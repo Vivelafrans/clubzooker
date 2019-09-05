357cc55375a67b7ed58652f7048549f2425a92ef
Frans Knaapen <fransknaapen@gmail.com>
2019-09-05 09:44:39 +0200

    Added meta tags and cover image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,12 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :address, :description, :photo])
     devise_parameter_sanitizer.permit(:account_update, keys: [:age, :address, :description, :photo])
+  end
+
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 end
