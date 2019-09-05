7473576cff75d900f441317df2b417870901a7dc
Levin Bauschke <b.bauschke@zeppelin-university.net>
2019-09-05 14:15:37 +0200

    The seed should work now apart frm geocode

--- app/models/sport.rb
+++ app/models/sport.rb
@@ -3,9 +3,10 @@ class Sport < ApplicationRecord
   has_many :offers
   has_many :scores
 
+  mount_uploader :photo, PhotoUploader;
+
   # validation for sport types must be inserted into name
   validates :name, presence: true
-  validates :teamsport, presence: true
   validates :intensity, presence: true, inclusion: { in: %w(low medium high),
     message: "%{value} is not a valid size" }
   validates :equipment, presence: true
--- db/schema.rb
+++ db/schema.rb
@@ -96,6 +96,12 @@ ActiveRecord::Schema.define(version: 2019_09_05_071622) do
     t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
+  create_table "search_suggestions", force: :cascade do |t|
+    t.string "term"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "sports", force: :cascade do |t|
     t.string "name"
     t.boolean "teamsport"
