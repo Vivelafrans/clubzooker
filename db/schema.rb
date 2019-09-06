7a4281c7a21c50fca3588f348bc9cd97188c474a
Frans Knaapen <fransknaapen@gmail.com>
2019-09-06 12:22:39 +0200

    Updated and removed running as a suggestion

--- db/schema.rb
+++ db/schema.rb
@@ -97,12 +97,6 @@ ActiveRecord::Schema.define(version: 2019_09_06_070614) do
     t.index ["user_id"], name: "index_scores_on_user_id"
   end
 
-  create_table "search_suggestions", force: :cascade do |t|
-    t.string "term"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "sports", force: :cascade do |t|
     t.string "name"
     t.boolean "teamsport"
